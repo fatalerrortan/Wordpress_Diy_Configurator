cec07d8d50d776e998e6013e15af5443c1b5c102
fatalerrortan <tiemann9898@gmail.com>
2016-11-21 00:50:23 +0100

    first push for init plugin

---
+++ diyconfigurator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Diy Configurator
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Development for integration of Diy Plattform
+Version: 1.0
+Author: xulin
+Author URI: http://www.xulin-tan.de
+*/
+?>
